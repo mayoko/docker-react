e0eb3f7e00828e87e3e04fc8efb0ef53c3feb51a
mnitta <mnitta@indeed.com>
2021-05-10 11:16:50 +0900

    updated app text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Bye, there!
+          Hi, there from feature branch!
         </p>
         <a
           className="App-link"
